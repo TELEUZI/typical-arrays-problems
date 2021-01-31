c60548107d77c4ad45649df9068a46170aa6497c
TELEUZI <41520794+TELEUZI@users.noreply.github.com>
2021-01-31 21:28:24 +0300

    feat: implement function to search mamimal and average value in array

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ exports.min = function min(array) {
         minValue > currentValue ? currentValue : minValue) : 0;
 };
 
-exports.max = function max(array) {};
+exports.max = function max(array) {
+    return isProperArray(array) ? array.reduce((maxValue, currentValue) =>
+        maxValue < currentValue ? currentValue : maxValue) : 0;
+};
 
-exports.avg = function avg(array) {};
+exports.avg = function avg(array) {
+    return isProperArray(array) ? array.reduce((acc, num) =>
+        acc + num, 0) / array.length : 0;
+};
 
-const isNotEmptyArray = (object) => object.length > 0;
-const isArray = (object) => Array.isArray(object);
-const isProperArray = (object) => isArray(object) && isNotEmptyArray(object);
+const isNotEmptyArray = object => object.length > 0;
+const isArray = object => Array.isArray(object);
+const isProperArray = object => isArray(object) && isNotEmptyArray(object);
